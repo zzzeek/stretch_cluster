1d9966be4ca43ebe7de7df1110dc02019d64a24b
zzzeek <mike_mp@zzzcomputing.com>
2017-12-13 18:15:27 -0500

    - edits

--- deploy_overclouds.sh
+++ deploy_overclouds.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
 
-SCRIPT_HOME=`dirname $0`
+DIRNAME=`dirname $0`
+SCRIPT_HOME=`realpath $DIRNAME`
+
+
 QUICKSTART_CONFIG=${SCRIPT_HOME}/oooq_config.yml
 
-export CHECKOUTS=${SCRPIPT_HOME}/checkouts
+export CHECKOUTS=${SCRIPT_HOME}/checkouts
 
 QUICKSTART1=${HOME}/.quickstart1
 QUICKSTART2=${HOME}/.quickstart2
 
-: ${CMDS:="cleanup run_undercloud run_overcloud build_hosts"}
+: ${CMDS:="setup_quickstart cleanup run_undercloud run_overcloud build_hosts"}
 
 if [[ ! $STACKS ]]; then
     echo "STACKS not defined, e.g. STACKS='stack1 stack2'"
@@ -19,7 +23,7 @@ HOSTS=${SCRIPT_HOME}/hosts
 RELEASE=master
 # RELEASE=pike
 
-
+set -e
 set -x
 
 setup_quickstart() {
@@ -27,11 +31,11 @@ setup_quickstart() {
     export QUICKSTART_CHECKOUT=${CHECKOUTS}/tripleo-quickstart
     export QUICKSTART_EXTRAS_CHECKOUT=${CHECKOUTS}/tripleo-quickstart-extras
 
-    if [ ! -d QUICKSTART_CHECKOUT ] then
+    if [ ! -d $QUICKSTART_CHECKOUT ]; then
         git clone https://git.openstack.org/openstack/tripleo-quickstart/ ${QUICKSTART_CHECKOUT}
     fi
 
-    if [! -d QUICKSTART_EXTRAS_CHECKOUT ] then
+    if [ ! -d $QUICKSTART_EXTRAS_CHECKOUT ]; then
         git clone https://git.openstack.org/openstack/tripleo-quickstart-extras/ ${QUICKSTART_EXTRAS_CHECKOUT}
         cd ${QUICKSTART_EXTRAS_CHECKOUT}
         for patchfile in `ls ${SCRIPT_HOME}/oooq-extras-patches/*.patch`
@@ -49,11 +53,11 @@ set_stack() {
    STACK=$1
 
    if [[ $STACK == "stack1" ]]; then
-        STACK_ARGS="${STACK1}"
-        QUICKSTART="${QUICKSTART1}"
+        STACK_ARGS="${STACK1_ARGS}"
+        QUICKSTART_ARG="${QUICKSTART1}"
    elif [[ $STACK == "stack2" ]]; then
-        STACK_ARGS="${STACK2}"
-        QUICKSTART="${QUICKSTART2}"
+        STACK_ARGS="${STACK2_ARGS}"
+        QUICKSTART_ARG="${QUICKSTART2}"
    else
         echo "no such stack ${STACK}"
         exit -1
@@ -63,14 +67,14 @@ set_stack() {
 }
 
 cleanup() {
-    rm -fr ${QUICKSTART}
+    rm -fr ${QUICKSTART_ARG}
 
-    grep ${STACK} /etc/passwd
-    if [ $? = 1 ]; then return; fi
+    user=`grep ${STACK} /etc/passwd | true`
+    if [ ! ${user} ]; then return; fi
 
     for name in undercloud control_0 control_1 control_2 compute_0 ; do
-        sudo -u ${STACK} virsh -c qemu:///session destroy ${name}
-        sudo -u ${STACK} virsh -c qemu:///session undefine ${name}
+        sudo -u ${STACK} virsh -c qemu:///session destroy ${name} || true
+        sudo -u ${STACK} virsh -c qemu:///session undefine ${name} || true
     done
 
     ps -ef | grep -e "^${STACK}" | cut -c 9-16 | xargs -n1 sudo kill -9
@@ -87,6 +91,9 @@ file://${QUICKSTART_EXTRAS_CHECKOUT}
 EOF
 
     export OOOQ_EXTRA_REQUIREMENTS=${SCRIPT_HOME}/extras-requirements.txt
+
+    export QUICKSTART_SCRIPT=${QUICKSTART_CHECKOUT}/quickstart.sh
+
     export CLEANALL="-T all -X"
 
     export UNDERCLOUD_TAGS="--tags untagged,provision,environment,libvirt,undercloud-scripts,undercloud-inventory,overcloud-scripts,undercloud-install,undercloud-post-install,overcloud-prep-config,overcloud-prep-containers,overcloud-prep-images,overcloud-prep-flavors,overcloud-prep-network"
@@ -94,7 +101,7 @@ EOF
     export SKIP_TAGS="--skip-tags overcloud-validate,tripleoui-validate,teardown-all,teardown-environment"
     export ALL_TAGS="--tags all"
 
-    export OPTS="${N_FLAG} -R ${RELEASE}  --config ${QUICKSTART_CONFIG} --nodes config/nodes/3ctlr_1comp.yml -e undercloud_disk=250  -e   control_memory=8192 -e undercloud_undercloud_nameservers=10.5.30.160 -e enable_pacemaker=true -e enable_port_forward_for_tripleo_ui=false -e tripleo_ui_secure_access=false"
+    export OPTS="-n -R ${RELEASE}  --config ${QUICKSTART_CONFIG} --nodes config/nodes/3ctlr_1comp.yml -e undercloud_disk=250  -e   control_memory=8192 -e undercloud_undercloud_nameservers=10.5.30.160 -e enable_pacemaker=true -e enable_port_forward_for_tripleo_ui=false -e tripleo_ui_secure_access=false"
 
     export STACK1_ARGS="-e rh_stack_name=stack1 -e rh_net_range_start=10 -e rh_net_range_end=80 -e ssh_user=stack1 -e non_root_user=stack1 -e  working_dir=/home/stack1 -e undercloud_user=stack1 -w ${QUICKSTART1}"
 
@@ -105,18 +112,18 @@ EOF
 
 
 run_undercloud() {
-    ${QUICKSTART} ${OPTS} ${UNDERCLOUD_TAGS} ${SKIP_TAGS} ${STACK_ARGS} 127.0.0.2
+    ${QUICKSTART_SCRIPT} ${OPTS} ${UNDERCLOUD_TAGS} ${SKIP_TAGS} ${STACK_ARGS} 127.0.0.2
 
 }
 
 run_overcloud() {
-    ${QUICKSTART} ${OPTS} ${OVERCLOUD_ONLY} ${SKIP_TAGS} ${STACK_ARGS} 127.0.0.2
+    ${QUICKSTART_SCRIPT} ${OPTS} ${OVERCLOUD_ONLY} ${SKIP_TAGS} ${STACK_ARGS} 127.0.0.2
 }
 
 build_hosts() {
    cat /dev/null > ${HOSTS}
    grep ${QUICKSTART1}/hosts -e "^stack1.*ansible" >> ${HOSTS}
-   grep ${QUICKSTART1}/hosts -e "^stack2.*ansible" >> ${HOSTS}
+   grep ${QUICKSTART2}/hosts -e "^stack2.*ansible" >> ${HOSTS}
 
    cat << EOF >> ${HOSTS}
 [master_overcloud]
@@ -143,7 +150,10 @@ EOF
 }
 
 
-setup_quickstart
+if [[ "${CMDS}" == *"setup_quickstart"* ]]; then
+    setup_quickstart
+fi
+
 setup_env
 for stack_arg in $STACKS ; do
     set_stack $stack_arg
@@ -163,7 +173,6 @@ for stack_arg in $STACKS ; do
 done
 
 if [[ "${CMDS}" == *"build_hosts"* ]]; then
-    echo "HI"
     build_hosts
 fi
 
