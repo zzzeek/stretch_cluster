3f6ae4e99777bc0b77c043b374cae8cc3e5564c0
zzzeek <mike_mp@zzzcomputing.com>
2018-04-16 17:02:43 -0400

    - galera now "runs" with pacemaker, however it is not creating the cluster.

--- roles/run-galera-containers/templates/setup_stretch_database.sh
+++ roles/run-galera-containers/templates/setup_stretch_database.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -x
 
-/usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
+if [ ! -d "/var/lib/mysql/mysql" ]
+then
+    /usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
+fi
 
-/usr/bin/mysqld_safe --skip-grant-tables &
 
+/usr/bin/mysqld_safe --port 3307 --skip-grant-tables --wsrep-provider=none &
+sleep 7
+
+# these connect on unix socket so port doesn't matter
 /usr/bin/mysql -u root -e "UPDATE mysql.user SET password=PASSWORD('{{ mysql_root_password }}') where user='root'; flush privileges;"
-/usr/bin/mysqladmin -u root --password={{ mysql_root_password }} -h localhost shutdown
+/usr/bin/mysqladmin -u root --password={{ mysql_root_password }}  shutdown
