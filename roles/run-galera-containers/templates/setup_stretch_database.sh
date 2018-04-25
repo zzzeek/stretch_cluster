8c5a59ba0b5f3bc30578f16e8d2424aba8a42096
zzzeek <mike_mp@zzzcomputing.com>
2018-04-25 15:59:59 -0400

    - fixes

--- roles/run-galera-containers/templates/setup_stretch_database.sh
+++ roles/run-galera-containers/templates/setup_stretch_database.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -x
 
-if [ ! -d "/var/lib/mysql/mysql" ]
-then
-    /usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
+if [ -d "/var/lib/mysql/mysql" ]; then
+    exit
 fi
+
+/usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
 
 
 /usr/bin/mysqld_safe --port {{ galera_listen_port }} --skip-grant-tables --wsrep-provider=none &
