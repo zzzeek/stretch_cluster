9072f3d7387dafe96fd45081b3a52bf9c9432cc6
Mike Bayer <mike_mp@zzzcomputing.com>
2018-05-04 11:48:55 -0400

    - clean up some names and make sure the keystone DB things only run once per overcloud

--- roles/run-galera-containers/templates/setup_stretch_database.sh
+++ roles/run-galera-containers/templates/setup_stretch_database.sh
@@ -8,9 +8,9 @@ fi
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
 
 
-/usr/bin/mysqld_safe --port {{ galera_listen_port }} --skip-grant-tables --wsrep-provider=none &
+/usr/bin/mysqld_safe --port {{ stretch_galera_listen_port }} --skip-grant-tables --wsrep-provider=none &
 sleep 7
 
 # these connect on unix socket so port doesn't matter
-/usr/bin/mysql -u root -e "UPDATE mysql.user SET password=PASSWORD('{{ galera_root_password }}') where user='root'; flush privileges;"
-/usr/bin/mysqladmin -u root --password={{ galera_root_password }}  shutdown
+/usr/bin/mysql -u root -e "UPDATE mysql.user SET password=PASSWORD('{{ stretch_galera_root_password }}') where user='root'; flush privileges;"
+/usr/bin/mysqladmin -u root --password={{ stretch_galera_root_password }}  shutdown
